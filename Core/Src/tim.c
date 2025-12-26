4730a78ee4aad6368e1d6c44f14dc06e0353b365
fuwaki <tiny.fly@outlook.com>
2025-12-26 18:49:46 +0800

    超频到96Mhz

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -93,7 +93,7 @@ void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 6719;
+  htim2.Init.Period = 7679;  // OVERCLOCK: 6719 -> 7679 to keep 12.5kHz PWM
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
@@ -186,7 +186,7 @@ void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 6719;
+  htim4.Init.Period = 7679;  // OVERCLOCK: 6719 -> 7679 to keep 12.5kHz PWM
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
